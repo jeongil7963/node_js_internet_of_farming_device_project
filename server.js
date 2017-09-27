d38b383f2dcdc7f557f0e17a995e21ec7dd97d24
양정일 <ya63kr@nate.com>
2017-09-27 14:22:36 +0900

    fix

--- server.js
+++ server.js
@@ -195,7 +195,7 @@ socket2.on(field_id, function(data){
     rst = camera.stop();
     console.log('socket2 : ' + data);
     camera.set("timelapse",1000);
-    console.log("rst" = rst);
+    console.log("rst : " +  rst);
 });
 
 socket2.on('disconnect', function(){
