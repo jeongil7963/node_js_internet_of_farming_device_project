0f56b793cf5208084d9cb0d1ef0b80d0e7a80890
양정일 <ya63kr@nate.com>
2017-10-10 16:05:38 +0900

    fix

--- server.js
+++ server.js
@@ -251,9 +251,9 @@ socket2.on('disconnect', function(){
 function executing_photo(){
     exec_photo(cmd_photo, function(error, stdout, stderr){
         console.log('Photo Saved : ', photo_path);
-        sending_photo();
     });
 };
+
 
 function sending_photo(){
     setTimeout(() => {
