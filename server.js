65630cbe7ab3b5dd6cf895ed80f1bf37c70aaa96
양정일 <ya63kr@nate.com>
2017-10-10 16:47:04 +0900

    fix

--- server.js
+++ server.js
@@ -232,7 +232,7 @@ socket2.on('connect', function(){
 socket2.on(field_id, function(data){
     if(data == "shoot")
     {
-        sending_photo();
+        shooting_photo();
     }
     else{
         console.log('web_socket : ' + data);
@@ -246,12 +246,14 @@ socket2.on('disconnect', function(){
 });
 
 // 사용자 직접 촬영
-
-
-function sending_photo(){
+function shooting_photo() {
     exec_photo(cmd_photo, function (error, stdout, stderr) {
         console.log('Photo Saved : ', photo_path);
     });
+}
+
+function sending_photo(){
+    
 
     console.log("sending photo");
     
