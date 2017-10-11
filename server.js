4a8fbb7effbd4dae063c467b9716ab9735b0e975
양정일 <ya63kr@nate.com>
2017-10-11 12:11:32 +0900

    fix

--- server.js
+++ server.js
@@ -140,7 +140,7 @@ camera.on("read", function(err, timestamp, filename) {
 function renaming_camera(){
     setTimeout(() => {
         fs.rename(
-            "./images/camera.jpg",  
+            "camera.jpg",  
             moment().format('YYYYMMDDHH')+".jpg", 
             function (err) { 
                 if (err) throw err; 
